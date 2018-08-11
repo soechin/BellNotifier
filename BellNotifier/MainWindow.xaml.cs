171cc489356195d0e1293fe9d584190789657525
Tzuchin Chang <soechin@gmail.com>
2018-08-11 14:17:11 +0800

    20180811_1417

--- BellNotifier/MainWindow.xaml.cs
+++ BellNotifier/MainWindow.xaml.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +28,8 @@ namespace BellNotifier
     {
         private string m_url;
         private Timer m_timer;
+        private string m_channel;
+        private SoundPlayer m_player;
 
         public MainWindow()
         {
@@ -36,6 +40,8 @@ namespace BellNotifier
         {
             m_url = "http://bd.youxidudu.com/db/api/get_gd.php?type=5";
             m_timer = new Timer(new TimerCallback(Timer_Elapsed), null, 0, 10000);
+            m_channel = null;
+            m_player = new SoundPlayer("bell.wav");
         }
 
         private void Window_Closed(object sender, EventArgs e)
@@ -70,13 +76,29 @@ namespace BellNotifier
             {
                 return;
             }
+
+            Dispatcher.BeginInvoke(new Action(delegate
+            {
+                ChannelText.Text = stru.Channel ?? "查無資料";
+                TimeText.Text = stru.Time;
+            }));
+
+            if (stru.Channel != m_channel)
+            {
+                m_channel = stru.Channel;
+
+                if (m_channel != null)
+                {
+                    m_player.Play();
+                }
+            }
         }
 
         private ResponseStruct MakeRequest()
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(m_url);
             string text;
-            dynamic json;
+            dynamic json, data;
             ResponseStruct stru;
 
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
@@ -94,6 +116,44 @@ namespace BellNotifier
                 Message = json["errmes"],
             };
 
+            data = json["data"];
+
+            if (data != null)
+            {
+                Regex regex = new Regex("([0-9]+)分([0-9]+)秒");
+                Match match;
+                string channel, time;
+                TimeSpan t1, t2;
+
+                t1 = TimeSpan.MinValue;
+
+                foreach (dynamic item in data)
+                {
+                    channel = item["xianlu"];
+                    time = item["GoldenBellTime"];
+
+                    if (t1 == TimeSpan.MinValue)
+                    {
+                        stru.Channel = channel;
+                        stru.Time = time;
+                    }
+
+                    match = regex.Match(time);
+
+                    if (match != null && match.Success)
+                    {
+                        t2 = new TimeSpan(0, int.Parse(match.Groups[1].Value),
+                            int.Parse(match.Groups[2].Value));
+
+                        if (t2 > t1)
+                        {
+                            stru.Channel = channel;
+                            stru.Time = time;
+                        }
+                    }
+                }
+            }
+
             return stru;
         }
     }
@@ -102,5 +162,7 @@ namespace BellNotifier
     {
         public string Code;
         public string Message;
+        public string Channel;
+        public string Time;
     }
 }
